b0e373bb9b6b57940beddd1803fbc553e299169f
Turtle1331 <turtle1331@users.github.com>
2018-10-23 12:56:45 -0700

    Add basic fullscreen and pointer lock, set charset

--- intro.js
+++ intro.js
@@ -1,20 +1,32 @@
+// Name uniforms here, appear as fields in uniforms object from initUniforms()
 const myUniforms = [
  "resolution",
  "time",
+ "mouse",
 ];
 
+
+
+// Sets uniforms at start of program
 function setupUniforms(gl, uniforms, vars) {
  const canvas = document.getElementById("canvas");
  gl.uniform2f(uniforms.resolution, canvas.width, canvas.height);
  gl.uniform1f(uniforms.time, vars.time);
+ gl.uniform2f(uniforms.mouse, 0, 0);
 }
 
+// Sets uniforms before rendering each frame
 function updateUniforms(gl, uniforms, vars) {
  gl.uniform1f(uniforms.time, vars.time);
 }
 
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
+
+
+// Call when page loads
 function main() {
  const canvas = document.getElementById("canvas");
  
@@ -25,34 +37,97 @@ function main() {
   return;
  }
  
+ // Initialize shader program and position buffer
  const shaderProgram = initShaderProgram(gl);
  const posBuffer = initBuffer(gl);
  
+ // Setup shader program with uniforms and vertices
  const uniforms = initUniforms(gl, shaderProgram, myUniforms);
  const program = {
   program: shaderProgram,
   vertices: gl.getAttribLocation(shaderProgram, "vPosition"),
   posBuffer: posBuffer
- };
- 
+ }; 
  prepareScene(gl, program, uniforms);
  
+ 
+ // Setup uniform vlaues
  const vars = {"time": 0, "dt": 0};
  setupUniforms(gl, uniforms, vars);
  
+ // Render loop
  function render(elapsed) {
+  // Calculate time values
   vars.dt = elapsed / 1000 - vars.time;
   vars.time = elapsed / 1000;
   
+  // Update uniform values
   updateUniforms(gl, uniforms, vars);
   
+  // Render to screen
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   requestAnimationFrame(render);
  }
+ // Loop
  requestAnimationFrame(render);
-
+ 
+ // Add fullscreen
+ fullScreen();
 }
 
+
+
+//
+function fullScreen() {
+ const canvas = document.getElementById("canvas");
+ 
+ // Find available fullscreen and pointerlock methods
+ canvas.requestFullscreen = canvas.requestFullscreen ||
+                            canvas.mozRequestFullScreen ||
+                            canvas.webkitRequestFullscreen;
+ 
+ document.exitFullscreen = document.exitFullscreen ||
+                           document.mozCancelFullScreen ||
+                           document.webkitExitFullscreen;
+ 
+ canvas.requestPointerLock = canvas.requestPointerLock ||
+                             canvas.mozRequestPointerLock ||
+                             canvas.webkitRequestPointerLock;
+ 
+ document.exitPointerLock = document.exitPointerLock ||
+                            document.mozExitPointerLock ||
+                            document.webkitExitPointerLock;
+ 
+ // Add pointer lock listener
+ document.addEventListener('pointerlockchange', pointerLockChange, false);
+ document.addEventListener('mozpointerlockchange', pointerLockChange, false);
+ document.addEventListener('webkitpointerlockchange', pointerLockChange, false);
+ 
+ // Add mouse listener
+ document.addEventListener("mousemove", onmousemove, false);
+ 
+ // Fullscreen and pointer lock on click
+ canvas.onclick = function() {
+  canvas.requestFullscreen();
+  canvas.onmousemove = canvas.requestPointerLock;
+ }
+}
+
+function pointerLockChange() {
+ // Check if pointer just locked or unlocked
+}
+
+function onmousemove() {
+ // pass
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+// Helper for linking shader programs
 function initShaderProgram(gl) {
  const vsh = loadShader(gl, gl.VERTEX_SHADER, vsh_src);
  const fsh = loadShader(gl, gl.FRAGMENT_SHADER, fsh_src);
@@ -72,6 +147,9 @@ function initShaderProgram(gl) {
  return shaderProgram;
 }
 
+
+
+// Helper for compiling shader source
 function loadShader(gl, type, src) {
  const shader = gl.createShader(type);
  
@@ -89,6 +167,9 @@ function loadShader(gl, type, src) {
  return shader;
 }
 
+
+
+// Helper for initializing position buffer
 function initBuffer(gl) {
   // Create a buffer for the square's positions.
  const positionBuffer = gl.createBuffer();
@@ -110,6 +191,9 @@ function initBuffer(gl) {
  return positionBuffer;
 }
 
+
+
+// Helper for finishing setup for rendering
 function prepareScene(gl, program, uniforms) {
  // Clear screen
  gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -126,6 +210,9 @@ function prepareScene(gl, program, uniforms) {
  const canvas = document.getElementById("canvas"); 
 }
 
+
+
+// Helper for initializing uniforms
 function initUniforms(gl, shaderProgram, uniformNames) {
  var uniforms = {};
  var location;
